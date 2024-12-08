662ee74c69756f4cea28918aaf55a821a7c63baf
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-08 21:09:04 +0600

    uri and client

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 
+const userName = process.env.USER_NAME;
+const userPass = process.env.USER_PASS;
+
+
 
 app.use(
   cors({
     origin: "http://localhost:5173/",
     methods: ["GET", "POST", "PATCH", "DELETE"],
-   
-
-  })
+   })
 );
 
 
 app.use(express.json())
+
+const uri =
+  `mongodb+srv://${userName}:${userPass}@cluster0.vrlyepl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
