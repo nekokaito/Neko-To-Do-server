c392c89435058f1606a92983ec96e9da45774313
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-08 21:38:00 +0600

    Posting and Getting User

--- index.js
+++ index.js
@@ -27,11 +27,62 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+const userCollection = client.db('neko-todo').collection('user');
+const todoCollection = client.db('neko-todo').collection('todo');
+
 const dbConnect = async () => {
   try {
     client.connect();
     console.log("DB Connected");
-  } catch {}
+
+    // Getting User
+
+    app.get("/user", async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
+
+    // Adding User
+    app.post("/add-user", async (req, res) => {
+      const { email, displayName, photoURL, uid } = req.body;
+
+      
+      const existingUser = await userCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      try {
+        
+        const result = await userCollection.insertOne({
+          email,
+          displayName,
+          photoURL,
+          uid,
+        });
+
+        res.status(201).json({ message: "User added successfully", result });
+      } catch (error) {
+        console.error("Error inserting user:", error);
+        res.status(500).json({ message: "Server error" });
+      }
+    });
+
+
+
+
+
+
+   // Try Ends Here
+
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 dbConnect();
